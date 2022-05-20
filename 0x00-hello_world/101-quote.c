642789427370b118ebd47ea56e822d3d9333ba38
magicaldave <smokydave@icloud.com>
2022-05-20 17:34:25 -0500

    Shortened syntax in 101-quote.c, changed variable to qte

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,8 @@
  */
 int main(void)
 {
-	static const char hello[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	fwrite(hello, sizeof(hello) - 1, 1, stderr);
+	char qte[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	fwrite(qte, sizeof(qte) - 1, 1, stderr);
 	return (1);
 }
