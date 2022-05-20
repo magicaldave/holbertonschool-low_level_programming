938299d546a164bd6fd1c35934ae7bfde8e0189e
magicaldave <smokydave@icloud.com>
2022-05-20 17:26:37 -0500

    Add: 101-quote.c which prints the second half of a quote to stderr, using fwrite

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	static const char hello[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	fwrite(hello, sizeof(hello) - 1, 1, stderr);
+	return (1);
+}
