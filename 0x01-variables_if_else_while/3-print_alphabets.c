e250dd489483bb2d13cb31b6ce730db7f5fdc548
magicaldave <smokydave@icloud.com>
2022-05-23 14:25:47 -0500

    Edit: added correct 3-alphabets file

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/*
+ * This C program outputs some simple strings using putchar
+ */
+
+/**
+ * main - prints the alphabet and a newline
+ * Return: a big integer and a trailing string
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+		putchar(ch);
+	for (ch = 'A' ; ch <= 'Z' ; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
