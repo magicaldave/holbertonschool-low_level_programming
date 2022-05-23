9fb8615141f63e36c7e702fe3749074f25da937f
magicaldave <smokydave@icloud.com>
2022-05-23 15:21:40 -0500

    Add: 9-print_comb which prints all decimal numbers, comma and space separated

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/*
+ * This C program outputs all combinations of single digits
+ */
+
+/**
+ * main - call var, print upwards
+ * Return: always 0
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = '0' ; ch <= '9' ; ch++)
+	{
+		putchar(ch);
+		if (ch != '9')
+			putchar(44);
+		putchar(32);
+}
+	putchar('\n');
+	return (0);
+}
