5215dfaade049864599c1e80157bc6fa38b51385
magicaldave <smokydave@icloud.com>
2022-05-23 10:52:16 -0500

    Edit: added comments to 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,18 +1,24 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
+/*
+ * This C program generates a random integer
+ * and evaluates if it is positive or negative.
+ */
 
-/* betty style doc for function main goes there */
+/**
+ * main - Generate random number based on time at half of RAND_MAX.
+ * Return: a big integer and a trailing string
+ */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if ( n > 0 )
+	if (n > 0)
 		printf("%d is positive\n", n);
-	else if ( n < 0 )
+	else if (n < 0)
 		printf("%d is negative\n", n);
 	else
 		printf("%d is 0\n", n);
