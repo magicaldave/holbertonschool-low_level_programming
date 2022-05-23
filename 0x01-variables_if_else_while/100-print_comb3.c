a30d87e817731dafa368292027a2eb43cd7ccd68
magicaldave <smokydave@icloud.com>
2022-05-23 17:33:44 -0500

    Edit: 100-print_comb3, added correct file

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/*
+ * This C program outputs all combinations of single digits
+ */
+
+/**
+ * main - call var, print upwards if var1 < var2
+ * Return: always 0
+ */
+int main(void)
+{
+	int ch;
+	int ch2;
+
+	for (ch = '0'; ch < '9'; ch++)
+	{
+		for (ch2 = '1' ; ch2 <= '9' ; ch2++)
+			if (ch < ch2)
+			{
+
+				putchar(ch);
+				putchar(ch2);
+				putchar(44);
+				putchar(32);
+			}
+		}
+	putchar('\n');
+	return (0);
+}
