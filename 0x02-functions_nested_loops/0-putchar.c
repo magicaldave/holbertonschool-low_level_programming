083b8e1386ed205649733030cee0b3b090ee7785
magicaldave <smokydave@icloud.com>
2022-05-24 12:19:26 -0500

    Edit: 0-putchar, edited to match Betty syntax

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,12 +6,14 @@
 
 /**
  * main - prints a string in an array via a loop
+ * Return: always 0
  */
 int main(void)
 {
 	int i;
 	char s[] = "_putchar";
 	int length = sizeof(s) / sizeof(s[0]);
+
 	for (i = 0 ; i < length ; i++)
 		_putchar(s[i]);
 	_putchar(10);
