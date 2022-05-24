54e118b400bf225abe5549e0bbdb8bde19d3eca2
magicaldave <smokydave@icloud.com>
2022-05-24 13:25:55 -0500

    Edit: 1-alphabet.c, cleaned according to betty syntax

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,11 +7,12 @@
 /**
  * main - prints a string in an array via a loop
  */
-void print_alphabet (void)
+void print_alphabet(void)
 {
 	int i;
 	char s[] = "abcdefghijklmnopqrstuvwxyz\n";
 	int length = (sizeof(s) - 1);
+
 	for (i = 0 ; i < length ; i++)
 		_putchar(s[i]);
 }
