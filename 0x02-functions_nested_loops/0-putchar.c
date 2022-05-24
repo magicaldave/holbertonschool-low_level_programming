59a3e32f494bd8f2f88a61d057ab1d0cf5473b7e
magicaldave <smokydave@icloud.com>
2022-05-24 13:10:17 -0500

    Edit: 0-putchar, changed syntax to avoid null terminator

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,14 +8,14 @@
  * main - prints a string in an array via a loop
  * Return: always 0
  */
+
 int main(void)
 {
 	int i;
-	char s[] = "_putchar";
-	int length = sizeof(s) / sizeof(s[0]);
+	char s[] = "_putchar\n";
+	int length = sizeof(s); /* / sizeof(s[0]);*/
 
-	for (i = 1 ; i < length ; i++)
+	for (i = 0 ; i < (length - 1) ; i++)
 		_putchar(s[i]);
-	_putchar(10);
 	return (0);
 }
