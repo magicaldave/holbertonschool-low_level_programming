a23f2b3045a8d9b3a2a993a7446f011098a43e6f
magicaldave <smokydave@icloud.com>
2022-05-24 13:10:57 -0500

    Edit: 1-alphabet.c, changed syntax to avoid null terminator

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -12,6 +12,6 @@ void print_alphabet (void)
 	int i;
 	char s[] = "abcdefghijklmnopqrstuvwxyz\n";
 	int length = sizeof(s)/sizeof(s[0]);
-	for (i = 0 ; i < length ; i++)
+	for (i = 0 ; i < (length - 1)  ; i++)
 		_putchar(s[i]);
 }
