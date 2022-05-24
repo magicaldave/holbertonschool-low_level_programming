346f948fc2fb3e517e1da2100ee889edf86aace7
magicaldave <smokydave@icloud.com>
2022-05-24 14:10:38 -0500

    Add: 3-islower.c which converts letters to type int and checks their case based on value; updated main.h

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/*
+ * This program uses a loop to output a lowercase alphabet
+ */
+
+/**
+ * print_alphabet_x10 - prints a string in an array via a loop, ten times
+ */
+int _islower(int c)
+{
+	if (c < 97)
+	{
+		return 0;
+			}
+	else
+	{
+		return 1;
+			}
+}
