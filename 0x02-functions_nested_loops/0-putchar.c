8e899508b1e307534970827dfffc48e38189728f
magicaldave <smokydave@icloud.com>
2022-05-24 12:18:09 -0500

    Edit: 0-putchar, removed newline from array

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,9 +10,10 @@
 int main(void)
 {
 	int i;
-	char s[] = "_putchar\n";
-	int length = sizeof(s)/sizeof(s[0]);
+	char s[] = "_putchar";
+	int length = sizeof(s) / sizeof(s[0]);
 	for (i = 0 ; i < length ; i++)
 		_putchar(s[i]);
+	_putchar(10);
 	return (0);
 }
