8b63fc4358b85904ee42e5fc0d0ed609cfdd6d87
magicaldave <smokydave@icloud.com>
2022-05-27 10:36:48 -0500

    Edit: 104-fibonacci.c, edited to meet Betty standards

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,3 +1,4 @@
+/*DAVECORLEY*/
 #include <stdio.h>
 /**
  * main - Print the first 98 fibonacci numbers space-separated
@@ -7,7 +8,6 @@
 int main(void)
 {
 	int count;
-
 	unsigned long p1x = 1;
 	unsigned long p2x = 0;
 	unsigned long sum = 0;
@@ -18,30 +18,27 @@ int main(void)
 		sum = (p1x + p2x);
 		p2x = p1x;
 		p1x = sum;
-			printf("%lu", sum);
-			putchar(44);
-			putchar(32);
+		printf("%lu, ", sum);
 	}
 	p1x_h1 = p1x / 10000000000;
 	p1x_h2 = p1x % 10000000000;
 	p2x_h1 = p2x / 10000000000;
 	p2x_h2 = p2x % 10000000000;
-
 	for (count = 92 ; count < 98 ; count++)
 	{
 		h1 = (p1x_h1 + p2x_h1);/*on each iteration, update both halves*/
 		h2 = (p1x_h2 + p2x_h2);
-	if (p1x_h2 + p2x_h2 > 9999999999) /*eliminate additional decimal place*/
-	{
+		if (p1x_h2 + p2x_h2 > 9999999999)
+		{
 		h1 += 1; /*increase ones value of first half if */
-		h2 %= 10000000000;/* the second overflows into it*/
-	}
+		h2 %= 10000000000;/* the second overflows into it on add'n*/
+		}
 		p2x_h1 = p1x_h1;
 		p2x_h2 = p1x_h2;
 		p1x_h1 = h1;
 		p1x_h2 = h2;
 		printf("%ld%ld", h1, h2);
-                if (count != 97)
+		if (count != 97)
 		{
 			putchar(44);
 			putchar(32);
