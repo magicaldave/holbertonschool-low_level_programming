712b41956adfdd4916bfb68b7fe066d4bc5d5ba3
magicaldave <smokydave@icloud.com>
2022-05-27 17:55:26 -0500

    Add: new folder, 0x04 - More functions & nested loops, add README file.

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/*
+ * This program uses a loop to output a lowercase alphabet
+ */
+
+/**
+ * _islower - converts characters to int and checks their case based on value
+ * Between a and z
+ * Return: 1 if lower alpha, 0 if anything else
+ * @c: char to convert in ASCII Decimal
+ */
+int _isupper(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (0);
+			}
+	else
+	{
+		return (1);
+			}
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,3 @@
+This folder contains project files related to 0x04 - More Functions, Nested Loops. They are as follows:
+1)0-isupper.c which checks the case of a given input char.
+2)
