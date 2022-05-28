8ae6f6c662ac731528590de1f164ca82f8abad85
magicaldave <smokydave@icloud.com>
2022-05-28 17:45:24 -0500

    Edit: 4-print_most_numbers.c, claened to meet betty standards

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,15 +1,16 @@
 #include "main.h"
 
 /**
- * print_numbers - writes all single digit characters to stdout
+ * print_most_numbers - writes all single digit characters to stdout except 2,4
  * Return: Always 0
  */
 void print_most_numbers(void)
 {
 	int c;
+
 	for (c = 48 ; c <= 57 ; c++)
 	{
-		if (c != 52 && c !=50)
+		if (c != 52 && c != 50)
 		_putchar(c);
 	}
 	_putchar(10);
