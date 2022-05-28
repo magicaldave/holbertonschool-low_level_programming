7b5d50dba6f6ae6e5852a939a5007d7b54225fd8
magicaldave <smokydave@icloud.com>
2022-05-28 16:15:30 -0500

    Add: 2-mul.c which multiplies two digit.

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/*
+ * Checks if inputs are digits or otherwise
+ */
+
+/**
+ * _isdigit - checks characters to see whether they are in the integer range
+ * Return: 1 if digit, 0 if otherwise
+ * @c: char to convert in ASCII Decimal
+ */
+int mul(int a, int b)
+{
+	return(a * b);
+}
