1a3834eba96afdbed8e791cef22836d90b9b5505
magicaldave <smokydave@icloud.com>
2022-05-28 17:12:58 -0500

    Add: 3-print_numbers.c, which prints all digits and a new line

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - writes all single digit characters to stdout
+ * Return: Always 0
+ */
+void print_numbers(void)
+{
+	int c;
+	for (c = 48 ; c <= 57 ; c++)
+	{
+		_putchar(c);
+	}
+	_putchar(10);
+}
