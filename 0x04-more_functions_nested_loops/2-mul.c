3c81eaac3520e1137c79eb867e6c59ec1fbb65ee
magicaldave <smokydave@icloud.com>
2022-05-28 17:50:14 -0500

    Add: 2-mul.c, cleaned to meet betty standards

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,15 +1,12 @@
 #include "main.h"
 
-/*
- * Checks if inputs are digits or otherwise
- */
-
 /**
- * _isdigit - checks characters to see whether they are in the integer range
+ * mul - multiplies two ints
  * Return: 1 if digit, 0 if otherwise
- * @c: char to convert in ASCII Decimal
+ * @a: int to mult
+ * @b: int to mult
  */
 int mul(int a, int b)
 {
-	return(a * b);
+	return (a * b);
 }
