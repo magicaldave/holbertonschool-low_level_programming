d24cf5ff8a0426a7d5ef0945a758bfd06691b146
magicaldave <smokydave@icloud.com>
2022-05-29 21:08:49 -0500

    Add: 9-fizz_buzz, which prints multiples of 3 and 5 up to 100, as fizz and buzz, or fizzbuzz for multiples of both

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * fizzbuzz - prints multiples of 3 and 5 up to 100
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1 ; i <= 100 ; i++)
+	{
+		if ((i % 3) == 0 && (i % 5 == 0))
+		{
+			printf("Fizzbuzz ");
+		}
+		else if ((i % 5 ) == 0)
+			printf("Buzz ");
+		else if ((i % 3) == 0)
+			printf("Fizz ");
+		else
+			printf("%d ", i);
+	}
+	putchar(10);
+	return (0);
+}
