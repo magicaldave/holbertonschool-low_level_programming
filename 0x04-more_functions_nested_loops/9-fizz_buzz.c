0a8d1d2303fe0a1ec00618723382dbc8b2ec26f1
magicaldave <smokydave@icloud.com>
2022-05-29 21:18:56 -0500

    Edit: 9-fizz_buzz, edited conditional logic for whitespace input

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,18 +13,18 @@ int main(void)
 	{
 		if ((i % 3) == 0 && (i % 5 == 0))
 		{
-			printf("Fizzbuzz ");
+			printf("Fizzbuzz");
 		}
 		else if ((i % 5) == 0)
 		{
 			printf("Buzz");
-			if (i != 100)
-				putchar(32);
 		}
 		else if ((i % 3) == 0)
-			printf("Fizz ");
+			printf("Fizz");
 		else
-			printf("%d ", i);
+			printf("%d", i);
+		if (i != 100)
+			putchar(32);
 	}
 	putchar(10);
 	return (0);
