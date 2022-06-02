d83f935d7d792e5d3c11574b6e6a618245260e4f
magicaldave <smokydave@icloud.com>
2022-06-02 13:24:21 -0500

    Edit: 7-puts_half.c, changed logic to filter out even numbers of characters

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,13 +9,14 @@
 void puts_half(char *s)
 {
 	int i;
+	int n = (_strlen(s) / 2);
 
-	for (i = (_strlen(s) / 2) ; i <= (_strlen(s) - 1) ; i++)
+	for (i = n ; i <= (_strlen(s) - 1) ; i++)
 	{
-		if (((_strlen(s) - 1) / 2) == 0)
+		if (((_strlen(s) - 1) % 2 ) == 1 && i == n )
 			continue;
 		else
-			_putchar(s[i]);
+		_putchar(s[i]);
 	}
 	_putchar(10);
 }
