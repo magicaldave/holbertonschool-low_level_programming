d9b931b1df04c2e8a3f9afa7310b9cd6361e602d
magicaldave <smokydave@icloud.com>
2022-06-06 13:56:16 -0500

    Edit: 3-strcmp.c, edited to meet betty syntax

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,9 +1,18 @@
+#include "holberton.h"
+
+/**
+ * _strcmp - compare two strings via ascii values
+ * @s1: string 1 to compare
+ * @s2: string 2 to compare
+ * Return: difference between strings
+ */
+
 int _strcmp(char *s1, char *s2)
 {
 	int i;
 	int diff = 0;
 
-	for ( i = 0 ; s1[i] != '\0' ; i++)
+	for (i = 0 ; s1[i] != '\0' ; i++)
 	{
 		if (s1[i] != s2[i])
 		{
