d0d37f7d0190f33361ac18e9093829673a56d312
magicaldave <smokydave@icloud.com>
2022-06-06 15:18:49 -0500

    Add: 4-reverse_array.c, which takes an input array and reverses its contents.

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * reverse_array - takes an input array and reverses it
+ * @a: array to reverse
+ * @n: charactyer count of array
+ * Return: always 0
+ */
+
+void reverse_array(int *a, int n)
+{
+	int count = 0, tmp;
+
+	n--;
+	while (n > 0)
+	{
+		tmp = a[n];
+		a[n] = a[count];
+		a[count] = tmp;
+		n--;
+		count++;
+	}
+}
+
