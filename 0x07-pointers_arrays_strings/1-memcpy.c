c05ed67b03525331fac66c3fa547a8887346f2ba
magicaldave <smokydave@icloud.com>
2022-06-07 15:59:30 -0500

    Edit: 1-memcpy.c, edited to meet Betty standards.

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/**
+ * _memcpy - compies n amount of one memory area to another
+ * @dest: destination memory area
+ * @src: source memory area to be copied
+ * @n: number of memory spaces to be copied
+ * Return: destination pointer, rewritten
+ */
+
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
