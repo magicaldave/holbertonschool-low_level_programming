db9cd5cfaa22dbe3f3b7763f70f13c1dd3f1ec38
magicaldave <smokydave@icloud.com>
2022-06-08 12:06:45 -0500

    Edit: 1-print_rev_recursion, edited to meet betty standards.

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+/**
+ * _print_rev_recursion - recursively prints a string in reverse
+ * @s: string to rev
+ * Return: always 0
+ */
+
 void _print_rev_recursion(char *s)
 {
 	int i = 0;
