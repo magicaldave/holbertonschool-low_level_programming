ccd55405dd49373b6ad2f3effe3fd00d9f0e3024
magicaldave <smokydave@icloud.com>
2022-06-08 14:34:38 -0500

    Edit: 3-factorial.c, edited to meet betty standards.

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,10 +1,16 @@
 #include "main.h"
 
+/**
+ * factorial - returns factorial of given int
+ * @i: int to factor
+ * Return: factorial of i
+ */
+
 int factorial(int i)
 {
 	if (i < 0)
 		return (-1);
-	if (i == 0)
+	else if (i == 0)
 		return (1);
 	return (i * factorial(i - 1));
 }
