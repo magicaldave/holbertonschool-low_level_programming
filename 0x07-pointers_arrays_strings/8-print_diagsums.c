11b2d273447ecf0bd8c674bab2d608967574ce4a
magicaldave <smokydave@icloud.com>
2022-06-07 23:42:48 -0500

    Edit: 8-print_diagsums.c, edited to meet betty standards.

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,6 +1,13 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * print_diagsums - reads a matrix and prits its diagonal sums
+ * @a: matrix to read
+ * @size: size of each row in matrix
+ * Return: always 0
+ */
+
 void print_diagsums(int *a, int size)
 {
 	int sum1 = 0, sum2 = 0, i;
