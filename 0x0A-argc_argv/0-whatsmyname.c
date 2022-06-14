b8cfc9ec24b456b9f0ed15deb86baee538e5fbcb
magicaldave <smokydave@icloud.com>
2022-06-14 18:51:50 -0500

    Edit: 0-whatsmyname.c, cleaned to meet betty standards

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,13 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * main - print the name of the program being ran
+ * @argc: number of args
+ * @argv: array of strings, with each place being one arg
+ * Return: Always 0
+ */
+
 int main(__attribute__ ((unused)) int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
