8ae9acce934382ff4e8bdfeaf4df5c0c9642da97
magicaldave <smokydave@icloud.com>
2022-06-16 14:59:19 -0500

    Edit: 0-create_array.c, shortened array length and removed conditional.

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,8 +12,7 @@ char *create_array(unsigned int size, char c)
 	char *array;
 	unsigned int i;
 
-	if (size != 0)
-		array = malloc((size + 1) * sizeof(char));
+	array = malloc(size * sizeof(char));
 	if (array != NULL)
 	{
 		for (i = 0 ; i < size ; i++)
