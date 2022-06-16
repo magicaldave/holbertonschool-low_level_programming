2244637d469ffe51e060cc171c5d185e9e24637d
magicaldave <smokydave@icloud.com>
2022-06-16 16:34:19 -0500

    Add: 2-str_concat.c, which dynamically allocates memory for a concatenated string and returns that string.

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * str_concat - concatenates two strings into a new one
+ * @s1: beginning of new string
+ * @s2: second half of new string
+ * Return: NULL on fail, or duped string
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int is1, is2, c;
+	char *dupe;
+
+	for (is1 = 0; s1[is1] != '\0'; is1++)
+	{
+	}
+	for (is2 = 0; s2[is2] != '\0'; is2++)
+	{
+	}
+	dupe = malloc((is1 + is2 + 1) * sizeof(char));
+	if (dupe != NULL)
+	{
+		for (c = 0 ; c < is1 ; c++)
+		{
+			*(dupe + c) = *(s1 + c);
+		}
+		for (c = 0 ; c < (is1 + is2) ; c++)
+		{
+			*(dupe + is1 + c) = *(s2 + c);
+		}
+	}
+	return (dupe);
+}
