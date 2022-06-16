0742dc9b88655016503c9f08bf0feed0f082970e
magicaldave <smokydave@icloud.com>
2022-06-16 15:56:50 -0500

    Add: 1-strdup.c, which duplicates an input string into dynamically created memory space.

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - duplicates an input string into a new one with dynamic memory
+ * @str: input string to dupe
+ * Return: NULL on fail, or duped string
+ */
+
+char *_strdup(char *str)
+{
+	int istr, c;
+	char *dupe;
+
+	for (istr = 0 ; str[istr] != '\0' ; istr++)
+	{
+	}
+	dupe = malloc(istr * sizeof(char));
+
+	if (str == NULL || dupe == NULL)
+		return (NULL);
+	for (c = 0 ; c < istr ; c++)
+	{
+		*(dupe + c) = *(str + c);
+	}
+	return (dupe);
+}
