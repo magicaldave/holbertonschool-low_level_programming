754fb8b93267ddcf69e7b917b92b10b1855e4c15
magicaldave <smokydave@icloud.com>
2022-06-16 14:56:01 -0500

    Add: new folder, 0x0B-Malloc_free, README file, and 0-create_array.c which builds an array with malloc and populates it.

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - builds an array of a specified size and initializes it
+ * @size: number of elements to store
+ * @c: character to initialize each element with
+ * Return: NULL if failed, otherwise pointer to new array
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *array;
+	unsigned int i;
+
+	if (size != 0)
+		array = malloc((size + 1) * sizeof(char));
+	if (array != NULL)
+	{
+		for (i = 0 ; i < size ; i++)
+		{
+			array[i] = c;
+		}
+		return (array);
+	}
+	return (NULL);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,6 @@
+This folder contains project files for 0x0C - Malloc, Free.
+They are as follows:
+
+1) 0-create_array, which builds an array with malloc and initializes each element of it.
+
+2) 
