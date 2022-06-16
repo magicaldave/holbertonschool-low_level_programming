bf9f5ac56f80fd34ec3d353b87fc23d78ab00f06
magicaldave <smokydave@icloud.com>
2022-06-16 15:37:59 -0500

    Edit: 0-create_array.c, removed unnecessary conditions.

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,13 +15,9 @@ char *create_array(unsigned int size, char c)
 	array = malloc(size * sizeof(char));
 	if (size == 0 || array == NULL)
 		return (NULL);
-	if (array != NULL)
+	for (i = 0 ; i < size ; i++)
 	{
-		for (i = 0 ; i < size ; i++)
-		{
-			array[i] = c;
-		}
-		return (array);
+		array[i] = c;
 	}
-	return (NULL);
+	return (array);
 }
