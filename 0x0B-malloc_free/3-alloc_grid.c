678308ed332f6b238f55aaca8017ce10cc4837ca
magicaldave <smokydave@icloud.com>
2022-06-16 20:42:15 -0500

    Edit: 3-alloc_grid.c, added error checking.

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -13,9 +13,20 @@ int **alloc_grid(int width, int height)
 	int **newgrid;
 	int x, y, i;
 
+	if (width <= 0 || height <= 0)
+		return (NULL);
 	newgrid = malloc(height * sizeof(int *));
-	for (i = 0 ; i < height ; i++)
-		newgrid[i] = malloc(width * sizeof(int));
+	if (newgrid != NULL)
+	{
+		for (i = 0 ; i < height ; i++)
+		{
+			newgrid[i] = malloc(width * sizeof(int));
+			if (newgrid[i] == NULL)
+				return (NULL);
+		}
+
+	}
+
 	for (x = 0 ; x < height ; x++)
 	{
 		for (y = 0 ; y < width; y++)
