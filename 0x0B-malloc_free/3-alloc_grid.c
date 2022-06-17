fe33f5e952faf17e5a69acf29726b30919be841e
magicaldave <smokydave@icloud.com>
2022-06-16 20:36:01 -0500

    Add: 3-alloc_grid.c, which allocates a 2-d array of memory. Needs some error checking.

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,12 +1,21 @@
 #include "main.h"
 #include <stdlib.h>
 
+/**
+ * alloc_grid - allocates a 2d array of memory
+ * @width: width of array
+ * @height: height of array
+ * Return: double-pointer to array
+ */
+
 int **alloc_grid(int width, int height)
 {
 	int **newgrid;
-	int x, y;
+	int x, y, i;
 
-	newgrid = malloc(((width + 1) * height) * sizeof(int));
+	newgrid = malloc(width * sizeof(int *));
+	for (i = 0 ; i < height ; i++)
+		newgrid[i] = (int *)malloc(height * sizeof(int));
 	for (x = 0 ; x < width ; x++)
 	{
 		for (y = 0 ; y < height; y++)
