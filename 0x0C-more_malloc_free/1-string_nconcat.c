d8ecca9ad499f3aaa4f79c04f36001776e42cdca
magicaldave <smokydave@icloud.com>
2022-06-21 18:48:08 -0500

    Edit: 1-string_nconcat.c, added null terminator at the end of string

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -36,5 +36,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (j = 0 ; (s2[j] != '\0') && (j < n) ; j++)
 		newstr[i + j] = s2[j];
 
+	newstr[i + j] = '\0';
+
 	return (newstr);
 }
