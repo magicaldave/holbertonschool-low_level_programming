5840a7a9c3f6c57869755fda6463597466f3786e
magicaldave <smokydave@icloud.com>
2022-06-21 17:48:20 -0500

    Edit: 0-malloc_checked.c, added comments.

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,6 +1,11 @@
 #include "main.h"
 #include <stdlib.h>
 
+/**
+ * malloc_checked - allocates memory and returns 98 on failure
+ * @b: precalculated size of memory to allocate
+ */
+
 void *malloc_checked(unsigned int b)
 {
 	int *newmem;
