5af904a3d404ea51f9c42302f4dd6653fa5e9800
magicaldave <smokydave@icloud.com>
2022-06-22 14:38:10 -0500

    Edit 2-print_dog.c, changed termination condition

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,21 +8,21 @@
  */
 void print_dog(struct dog *d)
 {
-	if (d)
-	{
-		if (!d->name)
-			printf("Name: (nil)\n");
-		else
-			printf("Name: %s\n", d->name);
+	if (d == NULL)
+		return;
 
-		if (!d->age)
-			printf("Age: (nil)\n");
-		else
-			printf("Age: %.6f\n", d->age);
+	if (!d->name)
+		printf("Name: (nil)\n");
+	else
+		printf("Name: %s\n", d->name);
 
-		if (!d->owner)
-			printf("Owner: (nil)\n");
-		else
-			printf("Owner: %s\n", d->owner);
-	}
+	if (d->age != d->age)
+		printf("Age: (nil)\n");
+	else
+		printf("Age: %.6f\n", d->age);
+
+	if (!d->owner)
+		printf("Owner: (nil)\n");
+	else
+		printf("Owner: %s\n", d->owner);
 }
