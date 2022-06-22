c45f88d187ecd42feb59d6d6d541602521c21b94
magicaldave <smokydave@icloud.com>
2022-06-22 15:23:40 -0500

    Edit: 4-new_dog.c, added comments.

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,15 @@
 #include "dog.h"
 #include <stdlib.h>
 
+/**
+ * new_dog - manually builds a dog struct and error-checks it
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ * Return: newdog pointer; NULL on failures
+ *
+ */
+
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	char *name_cpy, *owner_cpy;
