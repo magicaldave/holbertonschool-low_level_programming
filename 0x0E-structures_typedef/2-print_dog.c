0362b4bc5b3852e0cc435164de44348a20e79170
magicaldave <smokydave@icloud.com>
2022-06-22 14:20:42 -0500

    Edit: 2-print_dog.c, added colon to name printout

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,7 +13,7 @@ void print_dog(struct dog *d)
 		if (!d->name)
 			printf("Name: (nil)\n");
 		else
-			printf("Name %s\n", d->name);
+			printf("Name: %s\n", d->name);
 
 		if (!d->age)
 			printf("Age: (nil)\n");
@@ -23,6 +23,6 @@ void print_dog(struct dog *d)
 		if (!d->owner)
 			printf("Owner: (nil)\n");
 		else
-			printf("Owner: %s\n", (*d).owner);
+			printf("Owner: %s\n", d->owner);
 	}
 }
