080a35ae451534f265decf78d2a378ae38b0c726
magicaldave <smokydave@icloud.com>
2022-06-22 12:37:38 -0500

    Add: new folder, 0x0E - Structures, Typedef, README, dog.h which contains a struct about your/a dog!

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+#ifndef DOG
+#define DOG
+/**
+ * struct dog - details about your dog!
+ * @name: name of doggo
+ * @owner: name of owner. compatible with single- or double-parent households.
+ * @age: age of dog, accepting partial years
+ */
+struct dog
+{
+	char *name, *owner;
+	float age;
+};
+#endif /**DOG*/
