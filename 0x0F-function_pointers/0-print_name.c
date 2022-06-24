214148e6806592d95c1628ee086aaea5a673386f
magicaldave <smokydave@icloud.com>
2022-06-23 19:29:23 -0500

    Edit: 0-print_name.c, removed used of typedef

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -6,7 +6,7 @@
  * @f: function to call; no return, takes string (char pointer)
  */
 
-void print_name(char *name, print f)
+void print_name(char *name, void (*f)(char *))
 {
 	if (name != NULL)
 		f(name);
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,5 +2,5 @@
 #define MAIN
 /*Project 0*/
 typedef void (*print)(char *);/* new type for print functions */
-void print_name(char *name, print f); /* callback use for #0*/
+void print_name(char *name, void (*f)(char *)); /* callback use for #0*/
 #endif/**MAIN.H*/
