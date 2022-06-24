c7303fe500cde69234d90edbfaddc02a91904a19
magicaldave <smokydave@icloud.com>
2022-06-23 20:25:15 -0500

    Edit: 1-array_iterator.c, edited to meet betty standards.

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,5 +1,12 @@
 #include "function_pointers.h"
 
+/**
+ * array_iterator - call action on array
+ * @array: array to be acted upon
+ * @size: num elements in array
+ * @action: pointer (typedef'd) to passed function
+ */
+
 void array_iterator(int *array, size_t size, intaction action)
 {
 	unsigned int i;
