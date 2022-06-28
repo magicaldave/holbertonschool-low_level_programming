c89f3d4429825bfc66b8feec0b03ee6201f5ce09
magicaldave <smokydave@icloud.com>
2022-06-28 15:46:38 -0500

    Edit: 1-print_numbers.c, edited to meet betty syntax and accounted for additional edge cases.

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -2,17 +2,28 @@
 #include <stdio.h>
 #include "variadic_functions.h"
 
+/**
+ * print_numbers - prints a variable number of args as variadic input
+ * @separator: string to place between numbers
+ * @n: number of list inputs
+ * Return: N/A
+ */
+
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
 	va_list input;
 
 	va_start(input, n);
+
 	for (i = 0 ; i < n ; i++)
 	{
 		printf("%d", va_arg(input, int));
-		if (i != 3)
+		if (i < (n - 1) && separator)
 			printf("%s", separator);
 	}
+
 	va_end(input);
+
+	putchar(10);
 }
