81c1b9950ee0354b34025cc2ae76cc4444a58720
magicaldave <smokydave@icloud.com>
2022-07-08 22:01:16 -0500

    Add: 1-list_len.c, uses a macro'd for loop to count and return elements in a linked list.

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * list_len - gets total elements in a linked list
+ * @h: pointer to first element
+ * Return: # of elements printed
+ */
+
+size_t list_len(const list_t *h)
+{
+	int n;
+	const list_t *tmp;
+
+	readlinkedlist();
+
+	return (n);
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -4,6 +4,10 @@
 #include <stddef.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+
+/* Macro to read a linked list. only used for project #1 */
+#define readlinkedlist() for (n = 0, tmp = h ; tmp ; tmp = tmp->next, n++)
 
 /**
  * struct node - linked list item
@@ -19,4 +23,5 @@ typedef struct node
 } list_t;
 /*Function Prototypes*/
 size_t print_list(const list_t *h);
+size_t list_len (const list_t *h);
 #endif /* LISTS */
