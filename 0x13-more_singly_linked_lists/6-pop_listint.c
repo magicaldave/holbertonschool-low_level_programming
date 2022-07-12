36105907e9ee04cc8af41ed2280dee02a53b7545
magicaldave <smokydave@icloud.com>
2022-07-12 18:06:07 -0500

    Edit: 6-pop_listint.c, added betty compliant comments and changed int variable "n" to "data"

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,17 +1,23 @@
 #include "lists.h"
 
+/**
+ * pop_listint - deletes a linked list head and returns its data
+ * @head: first linked list element
+ * Return: data stored in deleted element
+ */
+
 int pop_listint(listint_t **head)
 {
 	listint_t *temp = *head;
-	int n = 0;
+	int data = 0;
 
 	if (*head)
 	{
-		n = temp->n;
+		data = temp->n;
 		*head = (*head)->next;
 	}
 
 	free(temp);
 
-	return (n);
+	return (data);
 }
