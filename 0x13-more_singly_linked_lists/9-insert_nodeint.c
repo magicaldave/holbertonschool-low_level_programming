fa7572be2ec1d562efa084e32333031b10fc6f23
magicaldave <smokydave@icloud.com>
2022-07-12 22:30:06 -0500

    Edit: 9-insert_nodeint.c, removed null temp handling which fails check 7.

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,7 +14,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new = malloc(sizeof(listint_t));
 	unsigned int i;
 
-	if (!new || !temp)
+	if (!new)
 		return (NULL);
 
 	new->n = n;
