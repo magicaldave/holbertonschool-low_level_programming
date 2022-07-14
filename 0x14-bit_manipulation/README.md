ae608eb522888f91bf20306fe4e6043dd4664c6f
magicaldave <smokydave@icloud.com>
2022-07-13 23:09:11 -0500

    Add: new folder, 0x14-bit_manipulation, README, 0-binary_to_uint.c, which converts binary input strings to unsigned ints

---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,6 @@
+This folder contains new project files relating to bit manipulation.
+They are as follows:
+
+0) binary_to_uint - converts binary inputs into unsigned ints
+
+1)
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,9 @@
+#ifndef BITS
+#define BITS
+/* Standard Libraries */
+#include <stddef.h>
+#include <string.h>
+/* Function Pointers */
+unsigned int __pow(unsigned int a, unsigned int b);
+unsigned int binary_to_uint(const char *b);
+#endif /* BITS */
