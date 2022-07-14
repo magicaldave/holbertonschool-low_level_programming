204dfc00eec8086e9a511b8640cc26e7184d652b
magicaldave <smokydave@icloud.com>
2022-07-13 23:50:59 -0500

    Add: 1-print_binary.c, which prints a binary representation of a number

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -3,7 +3,11 @@
 /* Standard Libraries */
 #include <stddef.h>
 #include <string.h>
+#include <unistd.h>
 /* Function Pointers */
 unsigned int __pow(unsigned int a, unsigned int b);
 unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+
+void _putchar(char c);
 #endif /* BITS */
