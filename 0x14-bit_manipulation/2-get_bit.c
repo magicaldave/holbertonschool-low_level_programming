c5d3d1ead5d16f7d3d9dc7a60d27f149a4e578a6
magicaldave <smokydave@icloud.com>
2022-07-13 23:54:20 -0500

    Add: remaining functions for 2-5, bit manipulation

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * get_bit - returns the value of a bit at a given index
+ * @n: number to find
+ * @index: the index you wish to calculate
+ * Return: value of bit at index or `-1` if an error occurs
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int i = 0;
+
+	if (n <= 1)
+		return (n % 2);
+
+	while (n >= 1)
+	{
+		if (i >= index)
+			break;
+		n /= 2;
+		i++;
+	}
+
+	if (index > i && index < sizeof(long int) * 8)
+		return (0);
+
+	if (index > sizeof(long int) * 8)
+		return (-1);
+
+	return (n % 2);
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -5,9 +5,12 @@
 #include <string.h>
 #include <unistd.h>
 /* Function Pointers */
-unsigned int __pow(unsigned int a, unsigned int b);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
 
 void _putchar(char c);
 #endif /* BITS */
