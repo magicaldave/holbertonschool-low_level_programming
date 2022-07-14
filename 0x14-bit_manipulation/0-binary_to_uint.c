ed62ba856115f1271431b601b7130ee68c3c2acd
magicaldave <smokydave@icloud.com>
2022-07-13 23:34:21 -0500

    Edit: 0-binary_to_uint, changed to use bit shifting instead of actual multiplication.

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,42 +8,24 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int i, power = 0, result = 0;
+	unsigned int power = 0, result = 0;
+	int i;
 
 	if (!b)
 		return (0);
 
-	for (i = 0; b[i] != '\0'; power++, i++)
+	for (i = 0; b[i] != '\0'; i++)
+	{
+	}
+
+	for (i--; i >= 0; power++, i--)
 	{
 		if (b[i] != 48 && b[i] != 49)
 			return (0);
 
 		if (b[i] == '1')
-			result += __pow(2, (strlen(b) - i - 1));
+			result += (1 << power);
 	}
 
 	return (result);
-}
-
-/**
- * __pow - clone of built-in pow function
- * @a: number to multiply
- * @b: power to multiply to
- * Return: result after multiplication
- */
-
-unsigned int __pow(unsigned int a, unsigned int b)
-{
-	unsigned int num = a, i = 1;
-
-	if (b == 0)
-		return (1);
-
-	while (i < b)
-	{
-		num *= a;
-		i++;
-	}
-
-	return (num);
 }
