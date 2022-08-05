978c2af7b44431cb38f19b5d9e3d7c9952ea0939
magicaldave <smokydave@icloud.com>
2022-08-04 23:57:51 -0500

    Add: 5-hash_table_print.c, which prints out an entire hash table

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,35 @@
+#include "hash_tables.h"
+#include <stdbool.h>
+/**
+ * hash_table_print - prints out all keys and values in a hash table
+ * @ht: hash table to print out
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *cmp = ht->array[0];
+	bool notover = false;
+
+	if (!ht || ht->size == 0)
+		return;
+
+	putchar('{');
+
+	for (i = 0; i < ht->size; i++, cmp = ht->array[i])
+	{
+		if (!cmp)
+			continue;
+
+		while (cmp)
+		{
+			if (notover)
+				printf(", ");
+			printf("'%s': '%s'", cmp->key, cmp->value);
+
+			cmp = cmp->next;
+
+			notover = true;
+		}
+	}
+	printf("}\n");
+}
