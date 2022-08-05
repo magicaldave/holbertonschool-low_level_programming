4a320fefc164cf7f3e03dba5d3e9edbbd8e57de0
magicaldave <smokydave@icloud.com>
2022-08-05 10:20:32 -0500

    Edit: moved conditions in 8-delete_dnodeint.c to check "head" then "*head" instead of the other way around.

--- 0x18-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x18-doubly_linked_lists/8-delete_dnodeint.c
@@ -12,7 +12,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	dlistint_t *nodeToDelete;
 	unsigned int i = 0;
 
-	if (!*head || !head)
+	if (!head || !*head)
 		return (-1);
 
 	temp = *head;
