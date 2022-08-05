e29c228012c871ada3e4c4146db177975ea49fd5
magicaldave <smokydave@icloud.com>
2022-08-04 20:13:17 -0500

    Edit: added comments to make betty-compliant

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,4 +1,11 @@
 #include "hash_tables.h"
+/**
+ * hash_table_set - sets an element of a hash table
+ * @ht: hash table object
+ * @key: search object to indicate @value
+ * @value: value to store per key
+ * Return: 1 on success, 0 on fail
+ */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	size_t i;
