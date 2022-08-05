f02ab2c081ef48ca23c4e8039c9358fa9676f4e7
magicaldave <smokydave@icloud.com>
2022-08-05 14:00:27 -0500

    Edit: 6-hash_table_delete.c, Added betty-compliant comments

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,4 +1,8 @@
 #include "hash_tables.h"
+/**
+ * hash_table_delete - frees all elements of @ht
+ * @ht: hash table to delete
+ */
 void hash_table_delete(hash_table_t *ht)
 {
 	unsigned long int i;
